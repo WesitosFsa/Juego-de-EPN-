a0b52aa292dce2d1217a29810dfe0dfba9343c6c
Kr-luis <kluis816@gmail.com>
2025-01-24 19:12:15 -0500

    añadir boton salir

--- core/src/main/java/com/teamkhaos/game/MenuPrincipal.java
+++ core/src/main/java/com/teamkhaos/game/MenuPrincipal.java
@@ -1,5 +1,8 @@
 package com.teamkhaos.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
@@ -10,10 +13,15 @@ import com.teamkhaos.game.utils.learn;
 public class MenuPrincipal extends Screens{
 
     ScrollPane scroll;
+    private Texture background;  // Imagen de fondo
+    private SpriteBatch batch;   // Para dibujar la imagen de fondo
 
     public MenuPrincipal(Main game) {
         super(game);
 
+        // Cargar la imagen de fondo
+        background = new Texture(Gdx.files.internal("android/assets/IMGgame/background_menu.gif"));
+        batch = new SpriteBatch();
         // Crear la tabla para el menu
 
         Table menu = new Table();
@@ -34,7 +42,16 @@ public class MenuPrincipal extends Screens{
             menu.row().padTop(20).height(50);
             menu.add(bt1).fillX();
         }
-
+        // Botón para salir del juego
+        TextButton exitButton = new TextButton("Salir", Assets.txtButtonMenu);
+        exitButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit(); // Cerrar la aplicación
+            }
+        });
+        menu.row().padTop(20).height(50);
+        menu.add(exitButton).fillX();
         scroll = new ScrollPane(menu, Assets.scrollPaneMenu);
         scroll.setSize(500, screen_height);
         scroll.setPosition(150, 0);
@@ -44,7 +61,14 @@ public class MenuPrincipal extends Screens{
 
     @Override
     public void draw(float delta) {
+        // Dibujar el fondo antes de los elementos de la UI
+        batch.begin();
+        batch.draw(background, 0, 0, screen_width, screen_height);
+        batch.end();
 
+        // Dibujar los elementos de la UI con Stage
+        stage.act(delta);
+        stage.draw();
     }
 
     @Override
@@ -74,6 +98,7 @@ public class MenuPrincipal extends Screens{
 
     @Override
     public void dispose() {
-
+        background.dispose();  // Liberar memoria de la textura
+        batch.dispose();
     }
 }
