a5fd07cb5b7f17006274307fa8c6327442cac0b6
Eduardo-Caza <ecaanaranjo@gmail.com>
2025-01-26 17:50:00 -0500

    arreglo de gameover

--- core/src/main/java/com/teamkhaos/game/GameOver.java
+++ core/src/main/java/com/teamkhaos/game/GameOver.java
@@ -10,6 +10,7 @@ public class GameOver extends Screens {
 
     public GameOver(Main game) {
         super(game);
+        hideButtons(true); // Ocultar los botones al cargar la pantalla de Game Over
         gameOverImage = new Texture(Gdx.files.internal("IMGgame/boxmapping/meme.png"));
         batch = new SpriteBatch();
     }
@@ -23,27 +24,27 @@ public class GameOver extends Screens {
 
     @Override
     public void update(float delta) {
-
+        // No se necesita actualizar nada por ahora
     }
 
     @Override
     public void show() {
-
+        // Código adicional si se necesita al mostrar la pantalla
     }
 
     @Override
     public void pause() {
-
+        // Código adicional si se necesita al pausar la pantalla
     }
 
     @Override
     public void resume() {
-
+        // Código adicional si se necesita al reanudar la pantalla
     }
 
     @Override
     public void hide() {
-
+        // Código adicional si se necesita al ocultar la pantalla
     }
 
     @Override
